7945f044878c6d0dc516587ef0449a8c3bc6ce2a
tonu4 <ankongupta312@gmail.com>
2025-01-06 19:51:35 +0600

    movesnake and drawfood function updated

--- snakeGame/snake.js
+++ snakeGame/snake.js
@@ -9,7 +9,7 @@ const Snakecolor="lightgreen";
 const SnakeBorder="blue";
 const foodcolor="red";
 const unitSize=25;
-let running=false; 
+let running=true; 
 let xVelocity=unitSize;
 let yVelocity=0;
 let foodx;
@@ -20,65 +20,10 @@ let snake=[
     {x:unitSize * 3, y:0},
     {x:unitSize * 2, y:0},
     {x:unitSize, y:0},
-    {x:0, y:0},
 ];
 
-// ctx.fillStyle = 'lightblue';
-// ctx.strokeStyle='black'
 
-// const boxArray=[
-//     {x:10,y:10},
-//     {x:20,y:10},
-//     {x:30,y:10},
-//     {x:40,y:10},
-//     {x:50,y:10}
-// ]
-
-// turnUpdown=(index)=>{
-//     boxArray[index].y+=10;
-// } 
-
-// turnRight=(size)=>{
-//     console.log(size)
-//     if(size<=0){
-//         for(i=0;i<5;i++){
-//             boxArray[i].y+=10;
-//         }
-//     }
-//     else{
-//         for(i=0;i<size;i++){
-//             boxArray[i].x+=10;
-//         }
-//     }
-// }
-
-// let turn=0;
-
-// drawbox=()=>{
-//     turnRight(4-turn);
-//     if(turn<=4){
-//         turnUpdown(4-turn);
-//     }
-//     turn++;
-
-//     for(i=0;i<5;i++){
-//         ctx.fillRect(boxArray[i].x,boxArray[i].y,10,10)
-//         ctx.strokeRect(boxArray[i].x,boxArray[i].y,10,10)
-//     }
-// }
-
-
-
-
-
-
-
-// setInterval(() => {
-//     ctx.clearRect(0,0,gameboard.width,gameboard.height);
-//     drawbox();
-// }, 100);
-
-window.addEventListener("keydown",chargeDirector);
+window.addEventListener("keydown",changeDirection);
 resertBtn.addEventListener("click",restartGame);
 
 
@@ -86,67 +31,100 @@ gameStart();
 
 
 function gameStart(){
-    const food={
+    const food=
+    {
         x:0,
-        y:0
+        y:0,
+        height:25,
+        width:25,
     }
     let score = 0;
     running=true; 
     scoreText.textcontent=score;
-    creatfood(food);
-    drawfood(food);
+    createfood();
+    drawsnake();
     nextTick();
 };
 function nextTick(){
     if(running){
         
-        setTimeout(()=>{
+        setInterval(()=>{
             clearboard();
-            drawfood();
-            movesnake();
+            createfood();
+            moveSnake();
             drawsnake();
-            checkGameover();
-            nextTick();
-        }, 75)
+            // checkGameover();
+            // nextTick();
+        }, 1000)
     }
     else{
         displayGameover();
     }
-};
+};0
+
 function clearboard(){
-    ctx.fillstyle=boardBackground;
+    ctx.fillStyle=boardBackground;
     ctx.fillRect(0,0,gamewidth,gameheight);
 };
 
-function creatfood(food){
+/**
+ * This fuction creates a food for the snake. 
+ * We have used Math.random function to  generate location of the food.
+ * @param {The food object which has the height and width of the food } food 
+ */
+function createfood(){
+
     function randomfood(min,max){
         const randNum=Math.round((Math.random() * (max-min)+min)/unitSize)*unitSize;
         return randNum;
     }
-    
-    food.x=randomfood(0, gamewidth-unitSize);
-    food.y=randomfood(0, gamewidth-unitSize);
-    
-};
-function drawfood(food){
-    ctx.fillstyle =foodcolor;
-    // ctx.fillRect(snakePart.x,snakePart.y,unitSize,unitSize);
-    
-    ctx.strokeRect(food.x,food.y,unitSize,unitSize);
-};
-function movesnake(){
 
+    x=randomfood(0, gamewidth-unitSize);
+    y=randomfood(0, gamewidth-unitSize);
+    ctx.fillStyle =foodcolor;
+    ctx.fillRect(x,y,25,25);
+    
 };
-function drawsnake(){
-    ctx.fillstyle=Snakecolor;
-    ctx.strokestyle=SnakeBorder;
-    snake.forEach(snakePart=>{
-        ctx.fillRect(snakePart.x,snakePart.y,unitSize,unitSize);
-        ctx.strokeRect(snakePart.x,snakePart.y,unitSize,unitSize);
+// function drawfood(food){
+//     ctx.fillstyle =foodcolor;
+//     // ctx.strokeRect(food.x,food.y,unitSize,unitSize);
+    
+// };
+/*
+let snake=[
+    {x:unitSize * 4, y:0},
+    {x:unitSize * 3, y:0},
+    {x:unitSize * 2, y:0},
+    {x:unitSize, y:0},
+];
+*/
+
+
+
+function moveSnake(){
+    console.log("hello")
+    snake.forEach((a)=>{
+        a.x+=unitSize;
     })
 };
-function chargeDirector(){};
+
+/**
+ * This function creates the snake.
+ * It takes the x,y values from the array we declared before.
+ */
+function drawsnake(){
+    ctx.fillStyle=Snakecolor;
+    ctx.strokeStyle=SnakeBorder;
+    snake.forEach(snakePart=>{
+        ctx.fillRect(snakePart.x,snakePart.y,25,25);
+        ctx.strokeRect(snakePart.x,snakePart.y,25,25);
+    })
+};
+
+function changeDirection(){};
 function checkGameover(){};
-function displayGameover(){};
+function displayGameover(){
+    console.log("gameover")
+};
 function restartGame(){};
 function resetGame(){};
